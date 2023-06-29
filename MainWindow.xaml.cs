a816e4fa1339824dfe3bd27531435c8cc8104c73
Tomrusz <Tomruszbiznes@gmail.com>
2023-06-29 19:01:03 +0200

    Dodaj pliki projektów.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Zadanie1_Tomasz_Ruszkowski
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
